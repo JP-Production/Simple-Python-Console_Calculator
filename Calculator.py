6d8ce22157dad774e3bb03e9052c768895332fde
unknown <janikpascal@freenet.de>
2021-12-28 00:25:35 +0100

    Added a little secret ;)

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,6 @@
+# imports
+import time
+
 # Shows a little string when the script is executed
 print("Welcome to my simple, python, console calculator. \nTo start just write: Calculator-Start or C-S for short | \nWrite /VOperators or /VO to get a list of valid Math Operations.\n")
 
@@ -6,13 +9,34 @@ command = str(input('Hello, User. What is your desired command >> '))
 
 # Gives you commands that can be written in the command prompt, power shell etc.
 def choose_command():
+    # Start the calculator
     if command == 'Calculator-Start' or command == 'C-S':
         number_one = float(input('Enter first number: '))
         operator = str(input('Enter Operation: '))
         number_two = float(input('Enter second number: '))
-        calculate(number_one, operator, number_two)           
+        calculate(number_one, operator, number_two)  
+    # Show a list of valid operations         
     elif command == '/VOperators' or command == '/VO':
         print('Valid Operators:\n {\n +, \n *, \n -, \n / \n }')
+    elif command == '/LOL':
+        lol(1)
+
+# Little secret if you dont look here ;)
+def lol(amount):
+    for n in range(amount):
+        time.sleep(.5)
+        print('lol')
+        time.sleep(.5)
+        print('this')
+        time.sleep(.5)
+        print('is')
+        time.sleep(.5)
+        print('a secret')
+        time.sleep(1)
+        print(':>')
+        time.sleep(1)
+        print("Okay not really if you looked at the code lol")
+
 
 # Calculates math operations
 def calculate(num1, op, num2):
